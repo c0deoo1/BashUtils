a3e0e95591475563ca9dff397582e188003193c5
Wanjie Liu <wanjie.liu@shopee.com>
2019-11-28 15:41:46 +0800

    fix cp.sh

--- cp.sh
+++ cp.sh
@@ -3,14 +3,14 @@ echo "mkdir $1"
 mkdir $1
 for img in `ls .`;
 do
-	filename=$(echo $img | cut -d . -f1)
-	extension=$(echo $img | cut -d . -f2)
-	if [ "$extension" = "png" ]
-	then
-		for resolution in 1x 2x 3x 1_5x;
-		do
-			newFile="${filename}_${resolution}.${extension}"
-			cp $img "$1\\${newFile}"
-		done
-	fi
+filename=$(echo $img | cut -d . -f1)
+extension=$(echo $img | cut -d . -f2)
+if [ "$extension" = "png" ]
+then
+    for resolution in 1x 2x 3x 1_5x;
+        do
+                newFile="${filename}_${resolution}.${extension}"
+                cp $img "$1/${newFile}"
+        done
+fi
 done
